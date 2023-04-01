f331f3d6431da4132644f9862b43266e5c365c67
namanchandak <naman.chandak@hotmail.com>
2023-04-01 10:15:41 +0530

    TweetBox done

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 // import Sidebar from '../client/components/Sidebar'
 
 import Sidebar from "../components/Sidebar"
-
+import Feed from "../components/home/Feed"
 const style={
   wrapper :`flex justify-center h-screen w-scren select-none bg-[#15202b] text-white`,
   content : `max-w-[1400px] w-2/3 flex justify-between`
@@ -17,7 +17,7 @@ export default function Home () {
       
       {/* <Sidebar/> */}
       <Sidebar/>
-      <h2>Feed</h2>
+      <Feed/>
       <h2>Widges</h2>
  
       </div>
